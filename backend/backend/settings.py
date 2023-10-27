6c2c2ff091310149edec3e138e712b08e5a0209d
Stas6671 <zaharenkostanislaw@yandex.ru>
2023-10-27 06:23:00 +0000

    Проверка коммитов.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['treetaskistas.sytes.net', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -103,7 +106,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -119,3 +124,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+sentry_sdk.init(
+
+    dsn="https://b982bd3015e6b79d542e938d6e922475@o4506115435659264.ingest.sentry.io/4506115446538240",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
